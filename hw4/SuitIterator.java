c95a0f2aba88fd4a4a1a5e3b1d5bf2d421f5177b
nobkog1220 <tinykogs@outlook.com>
2026-02-11 20:53:14 -0600

    Update SuitIterator.java

--- hw4/SuitIterator.java
+++ hw4/SuitIterator.java
@@ -1,14 +1,9 @@
-//DO NOT REMOVE PACKAGE LINE
-//Without this line authograder will not run correctly
-//You can comment it while you work on the problem
-//When everything works - uncomment and submit!
-//package com.gradescope.hw4;
+
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class SuitIterator implements Iterator<Card> {
-    //add instance variable/fields  as needed (suggested a list and node and suit)
     private DoubleNode<Card> current;
     private Deck d;
     private String suit;
@@ -24,12 +19,10 @@ public class SuitIterator implements Iterator<Card> {
             current = current.next;
         }
 
-        //put your code here
 
     }
     @Override
     public boolean hasNext() {
-        //put your code here
         return(current!=null);
 
     }
@@ -40,7 +33,6 @@ public class SuitIterator implements Iterator<Card> {
         //which in the ordered deck should be right after it
         //once the while loop stops we return the valid card we found
         //and our next valid card has been found to continue the iterator
-        //put your code here
         if(!hasNext()){
             throw new NoSuchElementException();
         }
