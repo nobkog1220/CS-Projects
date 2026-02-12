4d3d81893aeb995e429f4f412de1a37b398a3082
nobkog1220 <tinykogs@outlook.com>
2026-02-11 20:51:31 -0600

    Update BackwardsIterator.java

--- hw4/BackwardsIterator.java
+++ hw4/BackwardsIterator.java
@@ -1,14 +1,7 @@
-//DO NOT REMOVE PACKAGE LINE
-//Without this line authograder will not run correctly
-//You can comment it while you work on the problem
-//When everything works - uncomment and submit!
-//package com.gradescope.hw4;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 public class BackwardsIterator implements Iterator<Card> {
-    //Add instance variable/fields  as needed (suggested a list and node)
     private DoubleNode<Card> current;
     private Deck deck;
     private int size;
@@ -17,19 +10,16 @@ public class BackwardsIterator implements Iterator<Card> {
         this.deck = d;
         this.current = d.getLast();
         this.size = d.size();
-        //put your code here
     }
 
     @Override
     public boolean hasNext() {
-       //put your code here
        return(current!=null);
     }
 
     @Override
     public Card next() { //works similarly to topdown iterator but in this case
         //it starts at the end and goes in reverse
-        //put your code here
         if(!hasNext()){
             throw new NoSuchElementException();
         }
